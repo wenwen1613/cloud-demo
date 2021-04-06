8a2c45bd9044614be41201a81e8e6f8194e39c97
Oops <wenwen1613@163.com>
2021-04-06 20:05:30 +0800

    负载均衡+监控+健康检查

---
+++ eureka-server/src/main/java/org/example/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Oops
+ * @date 2021年04月06日 下午5:53
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
